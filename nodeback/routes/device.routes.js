0b1cbfa701d99b30c1af33dbcdc3699a76ecd8af
WARPSTAR ★ <alpay.gueruez@hsrw.org>
2025-07-22 15:56:02 +0200

    More Serverwork

--- nodeback/routes/device.routes.js
+++ nodeback/routes/device.routes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-router.get("/", (req, res) => {
-    //...
+router.get("/", async (req, res, next) => {
+    try {
+        const data = await fs.readFile("./data/devices", "utf-8");
+        const devices = JSON.parse(data);
+        res.status(200).send(devices);
+    } catch(err) {
+        next(err);
+    }
 });
 
 router.get("/:id", (req, res) => {
--- nodeback/server.js
+++ nodeback/server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const path = require('path');
+const errorHandler = require("./errorHandler");
+const logHandler = require("./logHandler");
+const PORT = 1000;
 
 app.use(express.json());
+app.use(logHandler);
 
-const userRoutes = require(".routes/user.routes");
-const deviceRoutes = require(".routes/device.routes");
-const lendingRoutes = require(".routes/lending.routes");
-const reservationRoutes = require(".routes/reservation.routes");
+const userRoutes = require("./routes/user.routes");
+const deviceRoutes = require("./routes/device.routes");
+const lendingRoutes = require("./routes/lending.routes");
+const reservationRoutes = require("./routes/reservation.routes");
 
 app.use("/users", userRoutes);
 app.use("/devices", deviceRoutes);
 app.use("/lendings", lendingRoutes);
 app.use("/reservations", reservationRoutes);
 
-const PORT = 1000;
+app.get("/", (req, res) => {
+    console.log("Request: Root")
+    res.status(200).send("NODEBACK läuft!");
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`NODEBACK läuft auf Port ${PORT}`));
