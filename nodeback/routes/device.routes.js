48e50455fc65be0b9cda88bdabd32e7e59bf16c1
WARPSTAR ★ <alpay.gueruez@hsrw.org>
2025-07-22 16:14:07 +0200

    JSON Test

--- nodeback/routes/device.routes.js
+++ nodeback/routes/device.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 
 router.get("/", async (req, res, next) => {
     try {
-        const data = await fs.readFile("./data/devices", "utf-8");
+        const data = await fs.readFile("./data/devices.json", "utf-8");
         const devices = JSON.parse(data);
-        res.status(200).send(devices);
+        res.status(200).json(devices);
     } catch(err) {
         next(err);
     }
