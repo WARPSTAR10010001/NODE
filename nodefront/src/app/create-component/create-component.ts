fa30390801e6c9cde28b000fbbf6f96730cc3e6b
WARP ★ <alpaygueruez@gmail.com>
2026-03-19 10:59:59 +0100

    V0.1.0 #9

--- nodefront/src/app/create-component/create-component.ts
+++ nodefront/src/app/create-component/create-component.ts
@@ -2,130 +2,206 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 
 import { DeviceService } from '../device-service';
-import { CategoryService, Category } from '../category-service';
-import { StatusService, Status } from '../status-service';
-import { LocationService, Location } from '../location-service';
-import { NetworkEnvironmentService, NetworkEnvironment } from '../network-environment-service';
-import { UserService, User } from '../user-service';
+import { Category, CategoryService } from '../category-service';
+import { Status, StatusService } from '../status-service';
+import { Location, LocationService } from '../location-service';
+import { NetworkEnvironment, NetworkEnvironmentService } from '../network-environment-service';
+
+type EntityType =
+  | 'device'
+  | 'category'
+  | 'status'
+  | 'location'
+  | 'network-environment';
 
 @Component({
   selector: 'app-create-component',
-  standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './create-component.html',
   styleUrls: ['./create-component.css']
 })
 export class CreateComponent implements OnInit {
-  selectedType: string = 'device';
-  form!: FormGroup;
-  submitting = false;
-
   categories: Category[] = [];
   statuses: Status[] = [];
   locations: Location[] = [];
-  networkEnvs: NetworkEnvironment[] = [];
-  users: User[] = [];
+  networkEnvironments: NetworkEnvironment[] = [];
+  entityType: EntityType = 'device';
+  form!: FormGroup;
+  submitting = false;
 
   constructor(
     private fb: FormBuilder,
-    private router: Router,
     private deviceService: DeviceService,
     private categoryService: CategoryService,
     private statusService: StatusService,
     private locationService: LocationService,
-    private networkService: NetworkEnvironmentService,
-    private userService: UserService
-  ) {}
+    private networkEnvService: NetworkEnvironmentService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
-    this.loadAllDropdowns();
-    this.initForm();
+  ngOnInit(): void {
+    this.loadLookups();
+    this.buildForm();
   }
 
-  loadAllDropdowns() {
-    this.categoryService.list().subscribe(res => this.categories = res.categories);
-    this.statusService.list().subscribe(res => this.statuses = res.statuses);
-    this.locationService.list().subscribe(res => this.locations = res.locations); // Prüfe hier im Network-Tab ob "houseNumber" geliefert wird
-    this.networkService.list().subscribe(res => this.networkEnvs = res.networkEnvironments);
-    this.userService.list().subscribe(res => this.users = res.users);
+  private loadLookups(): void {
+    this.categoryService.list().subscribe({
+      next: (res) => this.categories = res.categories,
+      error: (err) => console.error('Load categories failed', err)
+    });
+
+    this.statusService.list().subscribe({
+      next: (res) => this.statuses = res.statuses,
+      error: (err) => console.error('Load statuses failed', err)
+    });
+
+    this.locationService.list().subscribe({
+      next: (res) => this.locations = res.locations,
+      error: (err) => console.error('Load locations failed', err)
+    });
+
+    this.networkEnvService.list().subscribe({
+      next: (res) => this.networkEnvironments = res.networkEnvironments,
+      error: (err) => console.error('Load network envs failed', err)
+    });
   }
 
-  initForm() {
-    if (this.selectedType === 'device') {
-      this.form = this.fb.group({
-        inventoryNumber: ['', Validators.required],
-        name: ['', Validators.required],
-        manufacturer: [''],
-        model: [''],
-        serialNumber: [''],
-        categoryId: ['', Validators.required],
-        statusId: ['', Validators.required],
-        locationId: [null],
-        assignedToUserId: [null],
-        networkEnvironmentId: [null],
-        accountingType: ['konsumtiv'],
-        purchase: [null],
-        price: [null],
-        supplier: [''],
-        // Abschreibung
-        depreciationValue: [null], 
-        depreciationScale: ['years'],
-        // Netzwerk
-        ipAddress: [''],
-        macAddressesRaw: [''], // Hilfsfeld für String-Eingabe
-        patchPanelLabel: [''],
-        notes: ['']
-      });
-    } else {
-      this.form = this.fb.group({
-        name: ['', Validators.required],
-        description: ['']
-      });
+  onTypeChange(type: EntityType): void {
+    this.entityType = type;
+    this.buildForm();
+  }
+
+  private buildForm(): void {
+    switch (this.entityType) {
+      case 'device':
+        this.form = this.fb.group({
+          inventoryNumber: ['', [Validators.required, Validators.minLength(3)]],
+          name: ['', Validators.required],
+          manufacturer: [''],
+          model: [''],
+          serialNumber: [''],
+          categoryId: [null, Validators.required],
+          statusId: [null, Validators.required],
+          purchase: [''],
+          price: [null],
+          supplier: [''],
+          depreciationId: [null],
+          accountingType: ['konsumtiv', Validators.required],
+          locationId: [null],
+          networkEnvironmentId: [null],
+          patchPanelLabel: [''],
+          ipAddress: [''],
+          notes: ['']
+        });
+        break;
+
+      case 'category':
+        this.form = this.fb.group({
+          name: ['', Validators.required],
+          description: ['']
+        });
+        break;
+
+      case 'status':
+        this.form = this.fb.group({
+          name: ['', Validators.required],
+          description: ['']
+        });
+        break;
+
+      case 'location':
+        this.form = this.fb.group({
+          city: ['', Validators.required],
+          address: ['', Validators.required],
+          houseNumber: [''],
+          room: ['']
+        });
+        break;
+
+      case 'network-environment':
+        this.form = this.fb.group({
+          name: ['', Validators.required]
+        });
+        break;
     }
   }
 
-  onTypeChange(event: any) {
-    this.selectedType = event.target.value;
-    this.initForm();
+  get f() {
+    return this.form.controls;
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if (this.form.invalid || this.submitting) return;
+
     this.submitting = true;
 
-    let val = { ...this.form.value };
+    switch (this.entityType) {
+      case 'device':
+        this.deviceService.create(this.form.value).subscribe({
+          next: () => {
+            this.submitting = false;
+            this.router.navigate(['/devices']);
+          },
+          error: (err) => {
+            this.submitting = false;
+            console.error('Create device failed', err);
+          }
+        });
+        break;
 
-    if (this.selectedType === 'device') {
-      // Mac-Adressen von String zu Array konvertieren
-      val.macAddresses = val.macAddressesRaw ? val.macAddressesRaw.split(',').map((s: string) => s.trim()) : [];
-      delete val.macAddressesRaw;
-    }
+      case 'category':
+        this.categoryService.create(this.form.value).subscribe({
+          next: () => {
+            this.submitting = false;
+            this.router.navigate(['/devices']);
+          },
+          error: (err) => {
+            this.submitting = false;
+            console.error('Create category failed', err);
+          }
+        });
+        break;
 
-    let obs$: Observable<any> | undefined;
-    switch (this.selectedType) {
-      case 'device': obs$ = this.deviceService.create(val); break;
-      case 'category': obs$ = this.categoryService.create(val); break;
-      case 'status': obs$ = this.statusService.create(val); break;
-      case 'location': obs$ = this.locationService.create(val); break;
-      case 'network-environment': obs$ = this.networkService.create(val); break;
-    }
+      case 'status':
+        this.statusService.create(this.form.value).subscribe({
+          next: () => {
+            this.submitting = false;
+            this.router.navigate(['/devices']);
+          },
+          error: (err) => {
+            this.submitting = false;
+            console.error('Create status failed', err);
+          }
+        });
+        break;
 
-    if (obs$) {
-      obs$.subscribe({
-        next: () => {
-          this.submitting = false;
-          // Hier Overlay Erfolg
-          this.router.navigate(['/devices']);
-        },
-        error: (err) => {
-          this.submitting = false;
-          console.error('Fehler:', err);
-          // Hier Overlay Fehler (z.B. 404 wenn URL falsch oder 400 wenn Mac-Format falsch)
-        }
-      });
+      case 'location':
+        this.locationService.create(this.form.value).subscribe({
+          next: () => {
+            this.submitting = false;
+            this.router.navigate(['/devices']);
+          },
+          error: (err) => {
+            this.submitting = false;
+            console.error('Create location failed', err);
+          }
+        });
+        break;
+
+      case 'network-environment':
+        this.networkEnvService.create(this.form.value).subscribe({
+          next: () => {
+            this.submitting = false;
+            this.router.navigate(['/devices']);
+          },
+          error: (err) => {
+            this.submitting = false;
+            console.error('Create network env failed', err);
+          }
+        });
+        break;
     }
   }
 }
